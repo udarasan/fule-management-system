35f9f0285ef1156c768e8da7b96c40c50d5f1dd3
udarasan <udarassanjeewa@gmail.com>
2022-12-09 21:33:00 +0530

    new 6 - test cases 2

--- fule-management-system/src/test/java/lk/esoft/fulemanagementsystem/service/impl/FuelTokenServiceImplTest.java
+++ fule-management-system/src/test/java/lk/esoft/fulemanagementsystem/service/impl/FuelTokenServiceImplTest.java
@@ -1,11 +1,24 @@
 package lk.esoft.fulemanagementsystem.service.impl;
 
+import lk.esoft.fulemanagementsystem.dto.FuelTokenDTO;
+import lk.esoft.fulemanagementsystem.entity.FuelStation;
+import lk.esoft.fulemanagementsystem.entity.FuelToken;
+import lk.esoft.fulemanagementsystem.entity.Payment;
+import lk.esoft.fulemanagementsystem.entity.User;
+import lk.esoft.fulemanagementsystem.repository.FuelTokenRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class FuelTokenServiceImplTest {
 
     @BeforeEach
@@ -16,12 +29,38 @@ class FuelTokenServiceImplTest {
     void tearDown() {
     }
 
+    @Autowired
+    private FuelTokenServiceImpl fuelTokenService;
+
+    @Autowired
+    private FuelTokenRepository fuelTokenRepository;
+
+
     @Test
     void generateToken() {
+        FuelTokenDTO fuelTokenDTO=new FuelTokenDTO();
+        fuelTokenDTO.setTid(1);
+        fuelTokenDTO.setFillingTimeAndDate(Date.valueOf("2022-12-05"));
+        FuelStation fuelStation=new FuelStation();
+        fuelStation.setFid(1);
+        fuelTokenDTO.setFuelStationFk(fuelStation);
+        Payment payment = new Payment();
+        payment.setPid(1);
+        fuelTokenDTO.setPidFk(payment);
+        fuelTokenDTO.setRequestQuota(34);
+        User user=new User();
+        user.setUsername("udarasan");
+        fuelTokenDTO.setUsernameFk(user);
+        fuelTokenDTO.setVehicleRegNo(6129);
+        fuelTokenDTO.setStatus("ACCEPTED");
+        fuelTokenDTO.setTokenExpDate(Date.valueOf("2022-12-05"));
+        int res=fuelTokenService.generateToken(fuelTokenDTO);
+        assertEquals(res,404);
     }
 
     @Test
     void generateTokenInFirstTime() {
+
     }
 
     @Test
