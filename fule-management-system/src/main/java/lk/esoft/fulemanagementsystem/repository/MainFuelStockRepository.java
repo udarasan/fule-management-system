972a57e03e26a8203c1e52150688da3299ceb54e
udarasan <udarassanjeewa@gmail.com>
2023-01-04 16:42:06 +0530

    report details

--- fule-management-system/src/main/java/lk/esoft/fulemanagementsystem/repository/MainFuelStockRepository.java
+++ fule-management-system/src/main/java/lk/esoft/fulemanagementsystem/repository/MainFuelStockRepository.java
@@ -1,11 +1,9 @@
 package lk.esoft.fulemanagementsystem.repository;
 
-import lk.esoft.fulemanagementsystem.dto.MainFuelStockDTO;
 import lk.esoft.fulemanagementsystem.entity.MainFuelStock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface MainFuelStockRepository extends JpaRepository<MainFuelStock,Integer> {
-    List<MainFuelStockDTO> getMainStockDetails();
+
 }
